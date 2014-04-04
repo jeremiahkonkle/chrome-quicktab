ccb8a58f07db8524a51a255d5dbd76eb361ad9c6
Jeremiah Konkle <jeremiah@danjerous.com>
2014-04-03 19:53:10 -0600

    works! intercepts duplicate tab requests and gives option to open existing tab!

--- background.js
+++ background.js
@@ -1,21 +1,6 @@
 
 /*
   Copywrite 2013 Jeremiah Konkle
-
-  This file is part of QuickTab.
-
-  QuickTab is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  QuickTab is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with QuickTab.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 "use strict";
@@ -24,12 +9,59 @@
 // main bar
 var requests_filter = {urls: ["<all_urls>"], types: ['main_frame']}
 
-var check_for_open_tab = function(details) {
-  console.log("foo");
-  return {cancel: !confirm("Really open this url?")};
+var _tabs = [];
+var update_tabs = function() {
+  chrome.tabs.query({}, function(tabs) {
+    _tabs = tabs;
+  });
+}
+update_tabs();
+
+var _batch_timeout;
+var update_tabs_batched = function() {
+  if (_batch_timeout) {
+    clearTimeout(_batch_timeout);
+  }
+  _batch_timeout = setTimeout(update_tabs, 500);
+}
+
+chrome.tabs.onCreated.addListener(update_tabs_batched);
+chrome.tabs.onUpdated.addListener(update_tabs_batched);
+chrome.tabs.onMoved.addListener(update_tabs_batched);
+chrome.tabs.onDetached.addListener(update_tabs_batched);
+chrome.tabs.onAttached.addListener(update_tabs_batched);
+chrome.tabs.onRemoved.addListener(update_tabs_batched);
+chrome.tabs.onReplaced.addListener(update_tabs_batched);
+
+var check_for_open_tab = function(request) {
+  console.log("checking tabs: " + _tabs.length);
+  var match_url = request.url;
+  var result;
+  for (var i=0; i < _tabs.length; i++) {
+    if (_tabs[i].url == match_url && _tabs[i].id != request.tabId) {
+      result = _tabs[i];
+      break;
+    }
+  }
+  
+  console.log("found result");
+  if (result) {
+    if (confirm("This page is open in another tab. Open that tab?")) {
+      console.log("doing tab switch. closing tab: " + request.tabId 
+        + '. opening tab: ' + result.id
+        + '. opending window: ' + result.windowId);
+      chrome.windows.update(result.windowId, {focused: true});
+      chrome.tabs.update(result.id, {active: true});
+      if (request.tabId && request.tabId != result.id) {
+        chrome.tabs.remove(request.tabId);
+      }
+      return {cancel: true};
+    }
+  }
+  return {};
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
   check_for_open_tab, requests_filter, ['blocking']);
   
-console.log("bar");
+console.log("foo");
